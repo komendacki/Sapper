bd613ebfdf77a724f9fe2291df728626bb881d5c
komendacki <artur.komendacki@gmail.com>
2016-08-05 22:54:18 +0300

    Add receiveClick to GUI

--- GUI.java
+++ GUI.java
@@ -4,4 +4,11 @@
 public class GUI {
 
     private static Cell[][] cells;
+
+    public static int receiveClick(int x, int y, int button){
+        int cell_x = x/CELL_SIZE;
+        int cell_y = y/CELL_SIZE;
+
+        return cells[cell_x][cell_y].receiveClick(x, y, button);
+    }
 }
