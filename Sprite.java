3ff2d97b2336e1552708e4bb07e847b4bbf1dd84
komendacki <artur.komendacki@gmail.com>
2016-08-05 23:12:39 +0300

    Add texture names to Sprite enumerator

--- Sprite.java
+++ Sprite.java
@@ -2,4 +2,7 @@
  * Created by Artur Komendacki on 05.08.2016.
  */
 public enum Sprite {
+    ZERO("0"), ONE("1"), TWO("2"), THREE("3"), FOUR("4"), FIVE("5"), SIX("6"),
+    SEVEN("7"), EIGHT("8"), HIDEN("space"), BOMB("bomb"), EXPLOSION("explosion"),
+    FLAG("flag"), BROKEN_FLAG("broken_flag");
 }
